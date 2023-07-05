45a85831714647a437e05552fb5213d096b22c4e
usuariohugo <hugor8819@gmail.com>
2023-07-05 19:49:02 -0300

    primer version2

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
+    path("auth/", include("djoser.social.urls")),
+    
+    path("admin/", admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
